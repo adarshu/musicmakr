fc5df637370065ae0083a70f04c1d6de605ddd03
adarshu <adarshu@yahoo.com>
2014-05-17 23:31:09 -0700

    Added pubnub stuff

--- server/recv.php
+++ server/recv.php
@@ -1,5 +1,6 @@
 <?php
 require('twilio-php/Services/Twilio.php');
+require_once('lib/pubnub/Pubnub.php');
 
 $sid = "AC502a93adcdf4f27c65b7deb23027f094"; // Your Account SID from www.twilio.com/user/account
 $token = "4899636dd1ea3647709a78231da49b3d"; // Your Auth Token from www.twilio.com/user/account
@@ -33,6 +34,14 @@ if (startsWith(strtolower($body), "happy")) {
 }
 
 //file_put_contents("reply.txt", $resp . "\n");
+
+$pubnub = new Pubnub('pub-c-f1c6575f-d2d3-4cd1-bb38-2492c50b7d0e', 'sub-c-e877ab56-de0c-11e3-8c07-02ee2ddab7fe', 'sec-c-YzFiZTI4OWUtZjFmMS00Y2IzLTg1ZTYtYWYwMDEwZDllNWRh', false, false);
+$info = $pubnub->publish(array(
+    'channel' => 'hello_world', ## REQUIRED Channel to Send
+    'message' => '{"cmd":"play"}'   ## REQUIRED Message String/Array
+));
+print_r($info);
+
 
 // now greet the sender
 header("content-type: text/xml");
