c54ea0c4c27435c311b1791f901f69d881deb345
adarshu <adarshu@yahoo.com>
2014-05-18 00:01:35 -0700

    any song

--- server/recv.php
+++ server/recv.php
@@ -27,16 +27,16 @@ $resp = "default";
 $phone = $_REQUEST['From'];
 $body = $_REQUEST['Body'];
 
-if (startsWith(strtolower($body), "happy")) {
+//if (startsWith(strtolower($body), "happy")) {
     $resp = "Added to playlist";
     $pubnub = new Pubnub('pub-c-f1c6575f-d2d3-4cd1-bb38-2492c50b7d0e', 'sub-c-e877ab56-de0c-11e3-8c07-02ee2ddab7fe', 'sec-c-YzFiZTI4OWUtZjFmMS00Y2IzLTg1ZTYtYWYwMDEwZDllNWRh', false, false);
     $info = $pubnub->publish(array(
         'channel' => 'hello_world', ## REQUIRED Channel to Send
         'message' => array("cmd" => "search", "val" => "$body") ## REQUIRED Message String/Array
     ));
-} else {
-    $resp = "Incorrect";
-}
+//} else {
+//    $resp = "Incorrect";
+//}
 
 //file_put_contents("reply.txt", $resp . "\n");
 
